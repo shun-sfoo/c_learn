7ec0daf91b38db67f6f3bff06803343a3368c2a7
ibraid <ganymede0915@gmail.com>
2020-01-17 15:10:20 +0800

    gdb

---
+++ ten/reverse.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+  int i;
+  char str[6] = "hello";
+  char reverse_str[6] = "";
+
+  printf("%s\n", str);
+  for (i = 0; i < 5; i++)
+    reverse_str[4 - i] = str[i];
+  printf("%s\n", reverse_str);
+  return 0;
+}
