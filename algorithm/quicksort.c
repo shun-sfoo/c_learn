72f02d06906640e0a3025910a4053f4783434817
ibraid <ganymede0915@gmail.com>
2020-02-07 20:31:57 +0800

    gnu format

--- algorithm/quicksort.c
+++ algorithm/quicksort.c
@@ -2,42 +2,52 @@
 #include <assert.h>
 #include <stdio.h>
 
-int partition(int, int);
-void sort(int, int);
+int partition (int, int);
+void sort (int, int);
 
-int main(int argc, char *argv[]) {
-  sort(0, LEN - 1);
-  assert(is_sorted());
+int
+main (int argc, char *argv[])
+{
+  sort (0, LEN - 1);
+  assert (is_sorted ());
   int i;
-  for (i = 0; i < LEN; ++i) {
-    printf("%d ", a[i]);
-  }
+  for (i = 0; i < LEN; ++i)
+    {
+      printf ("%d ", a[i]);
+    }
   return 0;
 }
 
-int partition(int start, int end) {
+int
+partition (int start, int end)
+{
   int i = start, j = end, k = a[start], tmp;
-  while (i < j) {
-    while (i < end && a[i] <= k)
-      i++;
-    while (a[j] > k)
-      j--;
-    if (i < j) {
-      tmp  = a[i];
-      a[i] = a[j];
-      a[j] = tmp;
+  while (i < j)
+    {
+      while (i < end && a[i] <= k)
+        i++;
+      while (a[j] > k)
+        j--;
+      if (i < j)
+        {
+          tmp  = a[i];
+          a[i] = a[j];
+          a[j] = tmp;
+        }
     }
-  }
   tmp      = a[start];
   a[start] = a[j];
   a[j]     = tmp;
   return j;
 }
 
-void sort(int start, int end) {
-  if (start < end) {
-    int pivot = partition(start, end);
-    sort(start, pivot - 1);
-    sort(pivot + 1, end);
-  }
+void
+sort (int start, int end)
+{
+  if (start < end)
+    {
+      int pivot = partition (start, end);
+      sort (start, pivot - 1);
+      sort (pivot + 1, end);
+    }
 }
